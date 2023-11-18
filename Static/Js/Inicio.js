fec4766cef9abf2c8a8a7afc3ac51190971af005
Axelcano1731 <axelcano1711mail.com>
2023-11-18 15:25:13 -0500

    Agregar usuario funcional

--- Static/Js/Inicio.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-    $(".menu_hamburguesa img").click(function () {
-        $(".opciones ul").toggleClass("show");
-        $("body").toggleClass("menu_abierto");
-    });
-});
